323f9a6ad9a59b837f109a973101689c956a78fd
Vicente Correa da Silva Neto <vneto@tecgraf.puc-rio.br>
2019-03-07 16:59:17 -0300

    adjusts  alignment for time output

--- benchtime.cpp
+++ benchtime.cpp
@@ -25,12 +25,11 @@ int main(int argc, char *argv[])
     std::clock_t c_end = std::clock();
     auto t_end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "[" << arguments << "] (" << result << ")" << std::endl << 
-                std::fixed << std::setprecision(2) << "\tCPU time used: "
-                << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n"
-                << "\tWall clock time passed: "
-                << std::chrono::duration<double, std::milli>(t_end-t_start).count()
-                << " ms\n";
+    std::cout << std::setprecision(2) 
+              <<std::fixed 
+              << "[" << arguments << "] (" << result << ")" << std::endl
+              << "           CPU time used: " << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n"
+              << "  Wall clock time passed: " << std::chrono::duration<double, std::milli>(t_end-t_start).count() << " ms\n";
 
     return result;
   }
