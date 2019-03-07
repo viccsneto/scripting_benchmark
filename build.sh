8359fad54458d91db8c1ab068b2003d719e17fb8
Vicente Neto <vic.cs.neto@gmail.com>
2019-03-07 16:37:30 -0300

    Fixes lineend dos2unix Uses g++ instead of clang on build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-clang++ -O3 benchtime.cpp -m64 -march=x86-64 -o benchtime
-clang++ -O3 sum.cpp -m64 -march=x86-64 -o benchsum
-clang++ -O3 fibonacci.cpp -m64 -march=x86-64 -o benchfibonacci
+g++ -O3 benchtime.cpp -m64 -march=x86-64 -o benchtime
+g++ -O3 sum.cpp -m64 -march=x86-64 -o benchsum
+g++ -O3 fibonacci.cpp -m64 -march=x86-64 -o benchfibonacci
