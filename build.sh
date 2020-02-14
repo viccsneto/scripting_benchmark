97ccd1efdb4f5335dcd61ab4e7a10d76edf7ce43
Vicente Neto <vic.cs.neto@gmail.com>
2020-02-14 15:53:40 -0200

    Adds missing --std=c++11 flag for building on Linux

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-g++ -O3 benchtime.cpp -m64 -march=x86-64 -o benchtime
-g++ -O3 sum.cpp -m64 -march=x86-64 -o benchsum
-g++ -O3 fibonacci.cpp -m64 -march=x86-64 -o benchfibonacci
+g++ -O3 --std=c++11 benchtime.cpp -m64 -march=x86-64 -o benchtime
+g++ -O3 --std=c++11 sum.cpp -m64 -march=x86-64 -o benchsum
+g++ -O3 --std=c++11 fibonacci.cpp -m64 -march=x86-64 -o benchfibonacci
 javac Fibonacci.java
 javac Sum.java
